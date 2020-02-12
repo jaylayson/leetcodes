1f7d8bae00a6e04ce12bd04ab4b6642740a1c3ef
jaylayson <46077327+jaylayson@users.noreply.github.com>
2020-02-12 14:11:46 +1000

    added comments

--- number-of-steps-to-reduce-a-number-to-zero.java
+++ number-of-steps-to-reduce-a-number-to-zero.java
@@ -2,15 +2,18 @@ class Solution {
     public int numberOfSteps (int num) {
         int steps = 0;
         int newNum = num;
-        while(newNum != 0){
-            steps++;
+        while(newNum != 0){ //until newNum == 0
+            steps++; //increment counter
+			
+			//if newNum is even
             if(newNum%2 == 0){
                 newNum /= 2;
             }else{
+				//if newNum is odd
                 newNum -=1;
-            }
+            }//end of if-else
             
-        }
+        }//end of while
         return steps;
-    }
+    }//end of numberOfSteps
 }
